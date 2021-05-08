34de612f451807f5b289c0a23de5fd97de490783
yusupov <yusupov.islam@mail.ru>
2021-05-08 19:02:30 +0300

    usestate

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import Number from "./Number";
-import Buttons from "./Buttons";
+import React, {useState} from 'react';
+
+
 
 function Main(props) {
+    const[number, setNumber]=useState(0);
+    function plus() {
+        setNumber(number+1)
+    }
+    function minus() {
+        if (number>0){
+            setNumber(number-1)
+        }
+    }
+    function sbros() {
+        setNumber(0)
+    }
     return (
         <div>
-            <Number/>
-            <Buttons/>
+            <div className="number">
+                {number}
+            </div>
+            <div className="buttons">
+                <button className="button-1" onClick={plus}>Увеличить</button>
+                <button className="button-2" onClick={minus}>Уменьшить</button>
+                <button className="button-3" onClick={sbros}>Сбросить</button>
+            </div>
         </div>
     );
 }
